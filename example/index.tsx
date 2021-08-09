f556de8be29e3a103df8bad6c30c1f4ce35709cf
Vadzim Tsupryk <v.tsupryk@mageworx.com>
2021-08-09 19:49:25 +0300

    Update example

--- example/index.tsx
+++ example/index.tsx
@@ -8,9 +8,9 @@ import './styles.css';
 const App = () => {
   return (
     <div className="container">
-      <div className="row mt-5">
+      <div className="row mt-5 mb-5">
         <div className="col">
-          <h1 className="text-center">react-time-hook</h1>
+          <h1 className="text-center">@meecrobe/react-time-hook</h1>
         </div>
       </div>
       <div className="row justify-content-center">
@@ -22,6 +22,42 @@ const App = () => {
           <div className="mb-3">
             <label className="form-label"> 24-hour</label>
             <TimeInput value="12:30" format="24" />
+          </div>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col col-6 mt-3">
+          <div className="hstack gap-3 justify-content-center">
+            <a
+              href="https://github.com/meecrobe/react-time-hook"
+              className="link-dark"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                fill="currentColor"
+                role="img"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+            </a>
+            <a
+              href="https://www.npmjs.com/package/@meecrobe/react-time-hook"
+              className="link-dark"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="64"
+                height="64"
+                fill="currentColor"
+                role="img"
+                viewBox="0 0 576 512"
+              >
+                <path d="M288 288h-32v-64h32v64zm288-128v192H288v32H160v-32H0V160h576zm-416 32H32v128h64v-96h32v96h32V192zm160 0H192v160h64v-32h64V192zm224 0H352v128h64v-96h32v96h32v-96h32v96h32V192z"></path>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
